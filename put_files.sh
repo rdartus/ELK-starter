6ab9826fc899d4a121ba9fa335a97a70a832ab83
rdartus <richard.dartus@epfedu.fr>
2016-11-20 18:08:24 +0100

    merge

---
+++ put_files.sh
@@ -0,0 +1,4 @@
+cp -i ~/ELK-starter/configs/logstash-configs.conf /etc/logstash/conf.d
+cp -avr ~/ELK-starter/shop-around/index.html /usr/share/nginx/html/
+cp -avr ~/ELK-starter/shop-around/css/ /usr/share/nginx/html/
+cp -avr ~/ELK-starter/shop-around/js/ /usr/share/nginx/html/
--- web/put.sh
+++
@@ -1,3 +0,0 @@
-sudo rm /usr/share/nginx/html/index.html
-sudo cp -i ./index.html /usr/share/nginx/html
-sudo cp -i ./bootswatch.nested.css /usr/share/nginx/html
