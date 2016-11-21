ca7e0ac04d91a5c7475ebdeced96e4fccec0afd9
rdartus <richard.dartus@epfedu.fr>
2016-11-21 09:40:21 +0100

    htpasswd script

--- test.sh
+++ test.sh
@@ -1,8 +1,25 @@
-spawn htpasswd /etc/nginx/htpasswd.users test
+#!/usr/bin/expect
+#########################################
+#$ file: htpasswd.sh
+#$ desc: Automated htpasswd shell script
+#########################################
+#$
+#$ usage example:
+#$
+#$ ./htpasswd.sh passwdpath username userpass
+#$
+######################################
+
+set htpasswdpath [lindex $argv 0]
+set username [lindex $argv 1]
+set userpass [lindex $argv 2]
+
+# spawn the htpasswd command process
+spawn htpasswd $htpasswdpath $username
 
 # Automate the 'New password' Procedure
 expect "New password:"
-send "test2\r"
+send "$userpass\r"
 
 expect "Re-type new password:"
-send "test2\r"
+send "$userpass\r" 
