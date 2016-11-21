8abbc31617b614d6b5351cb96cc2e93a9dc5a142
rdartus <richard.dartus@epfedu.fr>
2016-11-21 01:54:41 +0100

    essais expect

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
-sudo echo -e "password\npassword\n" | htpasswd -c /etc/nginx/htpasswd.users 
+spawn htpasswd /etc/nginx/htpasswd.users test
+
+# Automate the 'New password' Procedure
+expect "New password:"
+send "test2\r"
+
+expect "Re-type new password:"
+send "test2\r"
